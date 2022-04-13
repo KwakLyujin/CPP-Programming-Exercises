2a7384b59c5fee23ad728607fdbecc95d0def885
KwakLyujin <102950231+KwakLyujin@users.noreply.github.com>
2022-04-13 12:07:31 +0900

    Add files via upload

---
+++ Week4_Homework/Week4_2/5x5_puzzlemain.cpp
@@ -0,0 +1,11 @@
+//메인함수
+
+#include "Ranking.h"
+extern int play5x5Puzzle();
+void main()
+{
+	loadRanking("ranking.txt");
+	int rank = play5x5Puzzle();
+	printRanking();
+	storeRanking("ranking.txt");
+}
