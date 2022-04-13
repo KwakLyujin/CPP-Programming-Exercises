4cff558b98fd7aaffd1d9c758d042779d0d774c6
KwakLyujin <102950231+KwakLyujin@users.noreply.github.com>
2022-04-13 12:22:48 +0900

    Add files via upload

---
+++ Week4_Homework/Week4_Homework1/4_4 game/maingame.cpp
@@ -0,0 +1,10 @@
+#include "Ranking.h"
+extern int playFifteenPuzzle();
+void main() {
+	loadRanking("ranking.txt");
+	int rank = playFifteenPuzzle();
+	printRanking();
+	storeRanking("ranking.txt");
+
+
+}
